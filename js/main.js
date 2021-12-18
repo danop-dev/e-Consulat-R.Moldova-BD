5a5516510d699e52a45e2cd2940eaac209e79409
Danu Oprea <opreadanu553@gmail.com>
2021-12-18 18:18:10 +0200

    Need add Login/Register

--- js/main.js
+++ js/main.js
@@ -1,25 +1,47 @@
-function ModalActivity() {
-    let links = document.getElementsByClassName('modal__item-link'),
-        closeBtn = document.getElementsByClassName('modal-close');
+$(document).ready(function () {
 
-    // Active Modal
-    for (let i = 0; i < links.length; i++) {
-        links[i].addEventListener('click', function() {
-            links[i].nextElementSibling.classList.add('show');
-            links[i].nextElementSibling.classList.add('active');
-            document.body.style.overflow = "hidden";
-        })
+    //Nav bar animation
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
+            $('.nav-bar').addClass("nav--toTop");
+        } else {
+            $('.nav-bar').removeClass("nav--toTop");
+        }
+
+    });
+
+    //Modal window action
+    function ModalActivity() {
+        let links = $('.modal__item-link'),
+            closeBtn = $('.modal-close');
+
+        // Active Modal
+        for (let i = 0; i < links.length; i++) {
+            links[i].addEventListener('click', function () {
+                links[i].nextElementSibling.classList.add('show');
+                links[i].nextElementSibling.classList.add('active');
+                document.body.style.overflow = "hidden";
+            })
+        }
+
+        // Disable Modal
+        for (let i = 0; i < closeBtn.length; i++) {
+            closeBtn[i].addEventListener('click', function () {
+                closeBtn[i].parentNode.classList.remove('active');
+                closeBtn[i].parentNode.classList.remove('show');
+                document.body.style.overflow = ('auto');
+
+            })
+        }
     }
+    ModalActivity(); // -> Run
 
-    // Disable Modal
-    for (let i = 0; i < closeBtn.length; i++) {
-        closeBtn[i].addEventListener('click', function() {
-            closeBtn[i].parentNode.classList.remove('active');
-            closeBtn[i].parentNode.classList.remove('show');
-            document.body.style.overflow = ('auto');
 
-        })
-    }
-}
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
 
-ModalActivity(); // -> Run
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 1500);
+    });
+});
